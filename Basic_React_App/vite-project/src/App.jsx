27a0eb0e5a9186c48557339d94b36013d7d66cb4
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-01-24 18:26:07 +0530

    commit

--- Basic_React_App/vite-project/src/App.jsx
+++ Basic_React_App/vite-project/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import StudentForm from './components/StudentForm'
 import RulesList from './components/RuleList'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+     <StudentForm></StudentForm>
      <RulesList></RulesList>
-        
+  
     </>
   )
 }
